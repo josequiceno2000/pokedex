cd91e316a7f3721055cc263d5e18b2401c7e7ba3
Jose Quiceno <divina.familia.nostri@gmail.com>
2025-03-21 11:41:24 -0500

    Add exit functionality

--- main.go
+++ main.go
@@ -1,10 +1,48 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
+type cliCommand struct {
+	name string
+	description string
+	callback func() error
+}
+
+
 func main()  {
-	fmt.Println("Hello, World!")
+	supportedCommands := map[string]cliCommand{
+		"exit": {
+			name: "exit",
+			description: "Exit the Pokedex",
+			callback: commandExit,
+		},
+	}
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Print("Pokedex > ")
+
+	for scanner.Scan() {
+		userInput := scanner.Text()
+		cleanedUserInput := cleanInput(userInput)
+		userCommand := cleanedUserInput[0]
+
+
+		command, ok := supportedCommands[userCommand]
+		if ok {
+			err := command.callback()
+			if err != nil {
+				fmt.Errorf("Error: %w", err)
+			}
+			break;
+		} else {
+			fmt.Errorf("Unknown command")
+		}
+		
+		fmt.Print("Pokedex > ")
+	}
 }
 
--- utils.go
+++ utils.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"strings"
+	"os"
 )
 
 func cleanInput(text string) []string {
 	text = strings.ToLower(strings.TrimSpace(text))
 	return strings.Fields(text)
+}
+
+func commandExit() error {
+	fmt.Printf("Closing the Pokedex... Goodbye!\n")
+	os.Exit(0)
+	return nil
 }
