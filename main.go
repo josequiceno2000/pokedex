98956cd40afa2b3e4fc25b75141c475112bba9de
Jose Quiceno <divina.familia.nostri@gmail.com>
2025-03-21 10:39:10 -0500

    Refactor and separate cleanInput

--- main.go
+++ main.go
@@ -2,22 +2,9 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
 func main()  {
 	fmt.Println("Hello, World!")
 }
 
-func cleanInput(text string) []string {
-	cleanOutput := []string{}
-	
-	if len(text) > 0 {
-		text = strings.Trim(text, " ")
-		text = strings.ToLower(text)
-		wordsToAdd := strings.Fields(text)
-		cleanOutput = append(cleanOutput, wordsToAdd...)
-	}
-	return cleanOutput
-
-}
