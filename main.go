f7f2df5213a3949f4b65740cba8de8a8130650b3
Jose Quiceno <divina.familia.nostri@gmail.com>
2025-03-26 12:17:27 -0500

    Refactor startRepl function

--- main.go
+++ main.go
@@ -1,24 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
-
 func main() {
-	
-	scanner := bufio.NewScanner(os.Stdin)
-
-	for {
-		fmt.Print("Pokedex > ")
-		
-		if scanner.Scan() {
-			userInput := scanner.Text()
-			cleanedInput := cleanInput(userInput)
-			fmt.Printf("Your command was: %v\n", cleanedInput[0])
-		}
-	}
-	
+	startRepl()	
 }
--- repl.go
+++ repl.go
@@ -2,7 +2,26 @@ package main
 
 import (
 	"strings"
+	"bufio"
+	"os"
+	"fmt"
 )
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan() 
+		
+		words := cleanInput(scanner.Text())
+		
+		if len(words) == 0 {
+			continue
+		}
+
+		fmt.Printf("Your command was: %v\n", words[0])
+	}
+}
 
 func cleanInput(text string) []string {
 	trimmedText := strings.TrimSpace(text)
