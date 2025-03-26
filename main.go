8ffd09dfb30bda0cb2913260623bfd2dd3c1bf6e
Jose Quiceno <divina.familia.nostri@gmail.com>
2025-03-26 12:07:45 -0500

    Create support for simple REPL

--- main.go
+++ main.go
@@ -1,10 +1,24 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 
 func main() {
+	
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		
+		if scanner.Scan() {
+			userInput := scanner.Text()
+			cleanedInput := cleanInput((userInput))
+			fmt.Printf("Your command was: %v", cleanedInput[0])
+		}
+	}
 	
 }
