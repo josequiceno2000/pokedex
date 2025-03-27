8f79b4abcfde5e529415738300ee9ca7b2f604ae
Jose Quiceno <divina.familia.nostri@gmail.com>
2025-03-26 20:13:37 -0500

    Renovate repl.go

--- repl.go
+++ repl.go
@@ -27,10 +27,14 @@ func startRepl(cfg *config) {
 		}
 
 		commandName := words[0]
+		args := []string{}
+		if len(words) > 1 {
+			args = words[1:]
+		}
 
 		command, exists := getCommands()[commandName]
 		if exists {
-			err := command.callback(cfg)
+			err := command.callback(cfg, args...)
 			if err != nil {
 				fmt.Println(err)
 			}
@@ -52,7 +56,7 @@ func cleanInput(text string) []string {
 type cliCommand struct {
 	name string
 	description string
-	callback func(*config) error
+	callback func(*config, ...string) error
 }
 
 func getCommands() map[string]cliCommand {
@@ -61,6 +65,11 @@ func getCommands() map[string]cliCommand {
 			name: "help",
 			description: "Displays a help message",
 			callback: commandHelp,
+		},
+		"explore": {
+			name: "explore",
+			description: "explore <location_name>",
+			callback: commandExplore,
 		},
 		"map": {
 			name: "map",
