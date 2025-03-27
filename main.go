bc23f6febef5258441b38723f077432aa7c66cce
Jose Quiceno <divina.familia.nostri@gmail.com>
2025-03-26 19:41:38 -0500

    Rework main.go

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"time"
+	"github.com/josequiceno2000/pokedex/internal/pokeapi"
+)
+
 func main() {
-	startRepl()	
+	pokeClient := pokeapi.NewClient(5 * time.Second)
+	cfg := &config{
+		pokeapiClient: pokeClient,
+	}
+
+	startRepl(cfg)	
 }
